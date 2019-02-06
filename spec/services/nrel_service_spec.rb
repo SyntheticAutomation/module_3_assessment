0dc9c1406d7c57f9976fc1e4d64720c7cb81b4d1
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-06 10:45:53 -0700

    Adjusts test suite to use VCR; all tests passing

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -4,15 +4,15 @@ describe 'NRELService' do
   describe 'stations' do
     it 'searches for stations' do
       VCR.use_cassette("services/find_stations") do
-        stations = NRELService.new(80203).find_stations_by_zip
+        stations = NRELService.new(80203).stations_by_zip
         station_1 = stations.first
 
         expect(stations.count).to eq(10)
-        expect(station_1.name).to eq("UDR")
-        expect(station_1.address).to eq("800 Acoma St")
-        expect(station_1.fuel_type).to eq("ELEC")
-        expect(station_1.distance).to eq(0.31)
-        expect(station_1.access_times).to eq("24 hours daily")
+        expect(station_1[:station_name]).to eq("UDR")
+        expect(station_1[:street_address]).to eq("800 Acoma St")
+        expect(station_1[:fuel_type_code]).to eq("ELEC")
+        expect(station_1[:distance].round(2)).to eq(0.31)
+        expect(station_1[:access_days_time]).to eq("24 hours daily")
       end
     end
   end
