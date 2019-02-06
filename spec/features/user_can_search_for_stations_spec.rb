8863145bbfb60d09576f29002003a5eceba8a7dc
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-06 09:33:42 -0700

    Adjusts test slightly for legibility

--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -8,22 +8,18 @@ describe 'A user of our web app' do
     expect(current_path).to eq(search_path)
     expect(page).to have_content("10 Closest Stations")
     expect(page).to have_css(".station", count: 10)
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    # And the stations should be limited to Electric and Propane
 
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
     within(first(".station")) do
       expect(page).to have_css(".name")
       expect(page).to have_css(".address")
-      expect(page).to have_css(".fuel_type")
+      expect(page).to have_css(".fuel-type")
       expect(page).to have_css(".distance")
-      expect(page).to have_css(".access_times")
+      expect(page).to have_css(".access-times")
     end
 
   end
 
 end
-
-# And I fill in the search form with 80203 (Note: Use the existing search form)
-# And I click "Locate"
-# Then I should be on page "/search"
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
