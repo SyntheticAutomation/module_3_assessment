526134e3c4e3f36849259e2d7693050d2662adbc
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-06 10:22:37 -0700

    Adds correct poro attributes after successful API call in Postman

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,11 +3,11 @@ class Station
   attr_reader :name, :address, :fuel_type, :distance, :access_times
 
   def initialize(attributes = {})
-    @name = attributes[:name]
-    @address = attributes[:address]
-    @fuel_type = attributes[:fuel_type]
-    @distance = attributes[:distance]
-    @access_times = attributes[:access_times]
+    @name = attributes[:station_name]
+    @address = attributes[:street_address]
+    @fuel_type = attributes[:fuel_type_code]
+    @distance = attributes[:distance].round(2)
+    @access_times = attributes[:access_days_time]
   end
 
 end
