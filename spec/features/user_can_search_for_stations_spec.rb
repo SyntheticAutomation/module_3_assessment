10d6edb7bdd1be4b4f7d6fb8c7e8d188e42729f2
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-06 10:47:55 -0700

    Wraps feature test in a vcr cassette block

--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -4,19 +4,20 @@ describe 'A user of our web app' do
   it 'can search for stations' do
     visit "/"
     fill_in "q", with: 80203
-    click_on "Locate"
-    expect(current_path).to eq(search_path)
-    expect(page).to have_content("10 Closest Stations")
-    expect(page).to have_css(".station", count: 10)
+    VCR.use_cassette("services/search_by_zip") do
+      click_on "Locate"
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content("10 Closest Stations")
+      expect(page).to have_css(".station", count: 10)
 
-    within(first(".station")) do
-      expect(page).to have_css(".name")
-      expect(page).to have_css(".address")
-      expect(page).to have_css(".fuel-type")
-      expect(page).to have_css(".distance")
-      expect(page).to have_css(".access-times")
+      within(first(".station")) do
+        expect(page).to have_css(".name")
+        expect(page).to have_css(".address")
+        expect(page).to have_css(".fuel-type")
+        expect(page).to have_css(".distance")
+        expect(page).to have_css(".access-times")
+      end
     end
-
   end
 
 end
